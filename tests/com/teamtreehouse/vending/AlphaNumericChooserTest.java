6429e6c77890a6e9fbd54596672b0f387155d815
Saif Tase <saif.tase@sulzer-us.com>
2018-03-06 14:05:56 -0500

    Added remaining tests, including new fixture for Bin class.

--- tests/com/teamtreehouse/vending/AlphaNumericChooserTest.java
+++ tests/com/teamtreehouse/vending/AlphaNumericChooserTest.java
@@ -1,13 +1,18 @@
 package com.teamtreehouse.vending;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.*;
 
 public class AlphaNumericChooserTest {
 
     private AlphaNumericChooser chooser;
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     @Before
     public void setUp() throws Exception {
@@ -33,5 +38,12 @@ public class AlphaNumericChooserTest {
     @Test(expected = InvalidLocationException.class)
     public void choosingLargerThanMaxIsNotAllowed() throws Exception {
         chooser.locationFromInput("B52");
+    }
+
+    @Test
+    public void constructingLargerThanAlphabetNotAllowed() throws Exception {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Maximum rows supported is 26");
+        new AlphaNumericChooser(27, 10);
     }
 }
